8ec85dbb99490bf69c8907bd67303cc3b44e1eaf
smantri7 <smantri7@gatech.edu>
2017-02-19 09:51:02 -0500

    Fixed Indentation
    Fixed indentation

--- RubyBookRater/app.rb
+++ RubyBookRater/app.rb
@@ -4,49 +4,49 @@ require 'sinatra/activerecord'
 require './config/environments'
 require './models/model'
 
-get '/' do
-	@title = 'Welcome to the Book Rater'
-	erb :index
+get '/'
+do@ title = 'Welcome to the Book Rater'
+erb: index
 end
 
 Choices = {
-'R' => "Romance",
-'A' => "Action",
-'H' => "Historical",
-'M' => "Mystery",
-'Ho' => "Horror",
-'O' => "Other",
+    'R' => "Romance",
+    'A' => "Action",
+    'H' => "Historical",
+    'M' => "Mystery",
+    'Ho' => "Horror",
+    'O' => "Other",
 }
 
-post '/cast' do 
-	@title = 'Thanks for casting your vote!'
-  	@vote  = params['vote']
-  	@store = YAML::Store.new 'votes.yml'
-  	@store.transaction do
-    @store['votes'] ||= {}
-    @store['votes'][@vote] ||= 0
-    @store['votes'][@vote] += 1
-  end
-  erb :cast
+post '/cast'
+do@ title = 'Thanks for casting your vote!'@
+vote = params['vote']@ store = YAML::Store.new 'votes.yml'@
+store.transaction do@ store['votes'] || = {}@
+    store['votes'][@vote] || =
+    0@ store['votes'][@vote] += 1
+end
+erb: cast
 end
 
-get '/results' do
-  @title = 'Results so far:'
-  @store = YAML::Store.new 'votes.yml'
-  @votes = @store.transaction { @store['votes'] }
-  erb :results
+get '/results'
+do@ title = 'Results so far:'@
+store = YAML::Store.new 'votes.yml'@
+votes = @store.transaction {@
+    store['votes']
+}
+erb: results
 end
 
-post '/submit' do
-  @model = Model.new(params[:model])
-  if @model.save
-    redirect '/models'
-  else
+post '/submit'
+do@ model = Model.new(params[: model])
+if@ model.save
+redirect '/models'
+else
     "Sorry, there was an error!"
-  end
+end
 end
 
-get '/models' do
-  @models = Model.all
-  erb :models
+get '/models'
+do@ models = Model.all
+erb: models
 end
--- RubyBookRater/db/migrate/20170218012708_create_model.rb
+++ RubyBookRater/db/migrate/20170218012708_create_model.rb
@@ -1,13 +1,12 @@
 class CreateModel < ActiveRecord::Migration
   def up
   	create_table :models do |t|
-  		t.string :name
-  		t.integer :rating
-  		t.string :comment
+  	    t.string :name
+  		  t.integer :rating
+  		  t.string :comment
   	end
   end
-
   def down
-  	drop_table :models
+      drop_table :models
   end
 end
